064c973a39debedc71e460f008ee382d7f37df8c
Nicolas Rouge <nicolas.rouge777@gmail.com>
2022-02-22 19:19:58 +0000

    many to many

--- Models/Character.cs
+++ Models/Character.cs
@@ -16,5 +16,7 @@ namespace netwebapi.Models
         public RpgClass Class { get; set; } = RpgClass.Knight;
         public User User { get; set; }
         public Weapon Weapon { get; set; }
+        public List<Skill> Skills { get; set; }
+
     }
 }
