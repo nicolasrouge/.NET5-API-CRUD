2051cbdecc84c769a2de37c2985a2d73019b03b8
Nicolas Rouge <nicolas.rouge777@gmail.com>
2022-05-28 18:38:34 +0100

    swagger even in prod

--- Startup.cs
+++ Startup.cs
@@ -64,12 +64,12 @@ namespace netwebapi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
+            //if (env.IsDevelopment())
+            //{
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "netwebapi v1"));
-            }
+            //}
 
             app.UseHttpsRedirection();
 
